d9a796af7ea77aa7e201a63263213bf10dbc6650
Joel Bonnie <74500069+joelbonnie@users.noreply.github.com>
2025-05-30 14:50:36 +0800

    Corrected mergeTwoSortedLinkedLists_21.py

--- LinkedLists/mergeTwoSortedLinkedLists_21.py
+++ LinkedLists/mergeTwoSortedLinkedLists_21.py
@@ -6,33 +6,27 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        if not list1:
-            return list2
-        elif not list2 
-            return list1 
-        head = None
-
-        if list1.val < list2.val:
-            head = list1
-            list1 = list1.next
-        else:
-            head = list2
-            list2 = list2.next
+        headNode = ListNode()
+        temp = headNode
 
         while list1 and list2:
+
             if list1.val < list2.val:
-                head.next = list1
+                temp.next = list1
                 list1 = list1.next
             else:
-                head.next = list2
+                temp.next = list2 
                 list2 = list2.next
-
-        while list1:
-            head.next = list1
+            temp = temp.next
+        
+        if list1:
+            temp.next = list1
             list1 = list1.next
-
-        while list2
-            head.next = list2
-            list2 = list2.next 
-
-        return head
+            temp = temp.next 
+        
+        elif list2:
+            temp.next = list2
+            list2 = list2.next
+            temp = temp.next 
+        
+        return headNode.next
