6e5672a90aafe9115f656c0c1120d77a1064a1d3
Joel Bonnie <joelbcasp@gmail.com>
2025-05-31 17:51:25 +0700

    May 31!

--- SlidingWindow/bestTimeToBuyAndSellStock_121.py
+++ SlidingWindow/bestTimeToBuyAndSellStock_121.py
@@ -1,3 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+        if len(prices) == 0:
+            return 0
+
+        maxProfit = 0 
+        minBuy = prices[0]
         
+        for currentPrice in prices:
+            currentProfit = currentPrice - minBuy
+
+            if currentProfit > maxProfit:
+                maxProfit = currentProfit 
+            
+            if currentPrice < minBuy:
+                minBuy = currentPrice 
+        
+        return maxProfit
+
+
