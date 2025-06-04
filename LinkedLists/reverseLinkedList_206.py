b76acbc0efdc2b8edf3388604eb859f6f4fbd091
Joel Bonnie <joelbcasp@gmail.com>
2025-06-04 17:09:49 +0800

    June 4 Added iterative reverse linked list!

--- LinkedLists/reverseLinkedList_206.py
+++ LinkedLists/reverseLinkedList_206.py
@@ -17,3 +17,14 @@ class Solution:
             head.next.next = head
         head.next = None
         return newHead
+
+    # Iterative
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head 
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr 
+            curr = temp
+        
+        return prev
