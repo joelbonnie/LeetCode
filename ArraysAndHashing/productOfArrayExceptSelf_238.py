1a4946361032541010c10602f1edc610b7a9e348
Joel Bonnie <joelbcasp@gmail.com>
2025-06-27 22:46:35 +0800

    June 27! Back at it

--- ArraysAndHashing/productOfArrayExceptSelf_238.py
+++ ArraysAndHashing/productOfArrayExceptSelf_238.py
@@ -1,23 +1,40 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #l_mul = 1
+        #r_mul = 1
+        #n = len(nums)
+
+        #prefix = [0] * n
+        #suffix = [0] * n 
+
+        #for i in range(n):
+        #    prefix[i] = l_mul 
+        #    l_mul *= nums[i]
+
+        #for i in range(n-1, -1, -1): 
+        #    suffix[i] = r_mul
+        #    r_mul *= nums[i]
+        #
+        #resultArr = []
+        #for i in range(n):
+        #    resultArr.append(prefix[i] * suffix[i])
+        #
+        #return resultArr
+
+
+        #O(1) space
         l_mul = 1
-        r_mul = 1
-        n = len(nums)
+        r_mul = 1 
+        resultArr = [0] * len(nums)
 
-        prefix = [0] * n
-        suffix = [0] * n 
-
-        for i in range(n):
-            prefix[i] = l_mul 
+        for i in range(len(nums)):
+            resultArr[i] = l_mul
             l_mul *= nums[i]
-
-        for i in range(n-1, -1, -1): 
-            suffix[i] = r_mul
+        
+        for i in range(len(nums) - 1, -1, - 1):
+            resultArr[i] *= r_mul
             r_mul *= nums[i]
         
-        resultArr = []
-        for i in range(n):
-            resultArr.append(prefix[i] * suffix[i])
-        
         return resultArr
+
 
